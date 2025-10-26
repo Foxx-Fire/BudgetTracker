e8a1c0ec10897a9e49e263dfe857ceb38a25189c
Olga Mikhailova <foxx_fire@mail.ru>
2025-10-27 00:49:43 +0300

    Create CategoryViewModel

---
+++ BudgetTracker/Sources/Modules/Common/ViewModels/CategoryViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryViewModel.swift
+//  BudgetTracker
+//
+//  Created by FoxxFire on 24.10.2025.
+//
+import Foundation
+
+final class CategoryViewModel: CategoryViewModelProtocol {
+    
+    private let categoryService: CategoryServiceProtocol
+    private var categories: [Category] = []
+    
+    init(categoryService: CategoryServiceProtocol = CategoryService()) {
+        self.categoryService = categoryService
+        self.categories = categoryService.allCategories
+    }
+    
+    var categoryCount: Int {
+        categories.count
+    }
+    
+    func getCategory(for index: Int) -> Category {
+        guard index < categories.count else {
+            return Category(name: "", icon: "")
+        }
+        return categories[index]
+    }
+    
+    func getCategory(for name: String) -> Category? {
+        return categoryService.category(for: name)
+    }
+}
