be9cba22eed7b8d3ad78be25e07e3988779caff5
Olga Mikhailova <foxx_fire@mail.ru>
2025-10-27 00:50:33 +0300

    Removed

--- BudgetTracker/Sources/Models/Category.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Category.swift
-//  Finance
-//
-//  Created by FoxxFire on 15.10.2025.
-//
-
--- BudgetTracker/Sources/Models/Transaction.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Transaction.swift
-//  Finance
-//
-//  Created by FoxxFire on 15.10.2025.
-//
-
